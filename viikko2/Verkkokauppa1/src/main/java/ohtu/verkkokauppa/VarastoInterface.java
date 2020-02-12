a1b94d099ffaf40f12f61e4931d0a2fced0c6427
Ilkka Ihalainen <ilkka.ihalainen@gmail.com>
2020-02-12 16:32:38 +0200

    Verkkokauppa1 (tehtävät 9-12)

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/VarastoInterface.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu.verkkokauppa;
+
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author iilkka
+ */
+
+@Component
+public interface VarastoInterface {
+
+    Tuote haeTuote(int id);
+
+    void otaVarastosta(Tuote t);
+
+    void palautaVarastoon(Tuote t);
+
+    int saldo(int id);
+    
+}
